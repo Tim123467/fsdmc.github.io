8c3fb2c987de6ba4528ea5be5f9ece2819b1a540
Jake <114749332+somerandomguyonhere@users.noreply.github.com>
2024-10-28 09:40:32 -0400

    Create js-popup.js
    added javascript for popup

---
+++ js-popup.js
@@ -0,0 +1,24 @@
+<script>
+    const popup = document.getElementById('popup');
+    const overlay = document.querySelector('.overlay');
+    const agreeButton = document.getElementById('agreeButton');
+
+    // show popup
+    function showPopup() {
+        popup.style.display = 'block';
+        overlay.style.display = 'block';
+    }
+
+    // hide popup
+    agreeButton.addEventListener('click', function() {
+        popup.style.display = 'none';
+        overlay.style.display = 'none';
+         // localStorage.setItem('popupDismissed', 'true'); 
+         // ^ remove once test succeed
+    });
+
+    // popup dismissed check
+    if (!localStorage.getItem('popupDismissed')) {
+        showPopup();
+    }
+</script>
